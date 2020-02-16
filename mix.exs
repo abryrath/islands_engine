2d59af3749b5a7fc08a36533a72114f1584b749b
Abry <abry@Abrys-Mac-mini.lan>
2020-02-16 14:37:18 -0500

    removing storage

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule IslandsEngine.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:toml, "~> 0.6"}
+      # {:toml, "~> 0.6"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
